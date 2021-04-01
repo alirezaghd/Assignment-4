56250ca4e35425f6c8611d7a07dd152c717ca6fe
alirezaghd <45558196+alirezaghd@users.noreply.github.com>
2021-04-01 18:48:10 +0430

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,9 +1,25 @@
 
+def safe(a,b):
+
+    for i in range(1, a+1):
+        if i % 2 == 0:    
+            for j in range(1,b+1):
+                if j % 2 == 0: 
+                    print('*', end='  ')
+                else:
+                    print('#', end='  ')
+            print()
+        else:
+            for j in range(1, b + 1):
+                if j % 2 == 0:
+                    print('#', end='  ')
+                else:
+                    print('*', end='  ')
+            print()
+
+
+
 a = int(input())
 b = int(input())
-for i in range(0, a):
-    for j in range(0,b):
-        i = '#'
-        j = '*'
-        print(i , j,end=(' '))
-        
+
+safe(a,b)
